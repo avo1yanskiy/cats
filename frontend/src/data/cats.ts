ea3eb8804b1b3cdd991c6f3b61ed73e8368fac37
handbook <avo1yanskiy@mail.ru>
2026-05-01 02:02:23 +0300

    fix

--- frontend/src/data/cats.ts
+++ frontend/src/data/cats.ts
@@ -10,21 +10,22 @@ export interface Cat {
 
 export const cats: Cat[] = [
   {
-    id: 'katya',
-    name: 'Катя',
-    age: '3 года',
-    breed: 'Домашняя короткошёрстная',
-    color: 'Рыжая с белыми пятнами',
-    description: 'Любит спать на клавиатуре и воровать еду со стола. Главная хозяйка дома.',
-    image: '/images/cat-katya.jpg',
+    id: "katya",
+    name: "Катя",
+    age: "4 года",
+    breed: "домашняя «дворянка»",
+    color: "Рыжая с белыми пятнами",
+    description: "Психованная дама, пугливая до чертиков",
+    image: "/images/cat-katya.jpg",
   },
   {
-    id: 'gavrik',
-    name: 'Гаврик',
-    age: '4 года',
-    breed: 'Британская короткошёрстная',
-    color: 'Серый',
-    description: 'Уличный боец, но боится пылесоса. Самый ласковый кот в мире.',
-    image: '/images/cat-gavrik.jpg',
+    id: "gavrik",
+    name: "Гаврик",
+    age: "4 года",
+    breed: "домашний, «дворянин»",
+    color: "Серый/белый",
+    description:
+      "Попрошайка каких свет не видовал, не боится пылесоса. Самый ласковый кот в мире.",
+    image: "/images/cat-gavrik.jpg",
   },
 ];
